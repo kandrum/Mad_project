e9347dde39ab64afd8842478a8be6d0d0db1e307
kandrum <kandrum5418@uhcl.edu>
2023-10-16 21:43:44 -0500

    Basic Design

---
+++ FlightSearch/FlightSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlightSearch
+//
+//  Created by Kandru, Mounika Preethi on 10/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
