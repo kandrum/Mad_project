615941517cbaedad0a2624efcdf6ad5201356673
yaswanthzz <143757849+yaswanthzz@users.noreply.github.com>
2023-12-04 19:23:22 -0600

    UI elements added for one way to sort flights based on shortest duration first

--- FlightSearch/FlightSearch/OneWayDisplayViewController.swift
+++ FlightSearch/FlightSearch/OneWayDisplayViewController.swift
@@ -8,98 +8,98 @@
 import UIKit
 
 class OneWayDisplayViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
-   
+    
     var selectedFlightInfo: DisplayFlightInfo?
-
+    
     struct FlightInfo: Codable {
-            let legs: [Leg]
-            let trips: [Trip]
-            let fares: [Fare]
-            let airlines: [Airline]
-            let airports: [Airport]
-            // Add other fields as needed
-        }
-
-        struct Leg: Codable {
-            let id: String
-            let departureTime: String
-            let arrivalTime: String
-            let duration: String
-            let departureAirportCode: String
-            let arrivalAirportCode: String
-            let airlineCodes: [String]
-            let stopoverAirportCodes: [String]
-            let allianceCodes: [String]
-            let stopoversCount: Int
-            let segments: [Segment]
-            // Add other fields as needed
-        }
-
-        struct Segment: Codable {
-            let durationMinutes: Int
-            let stopoverDurationMinutes: Int
-            let departureAirportCode: String
-            let arrivalAirportCode: String
-            let airlineCode: String
-            let cabin: String
-            let designatorCode: String
-            // Add other fields as needed
-        }
-
-        struct Trip: Codable {
-            let id: String
-            let code: String
-            let legIds: [String]
-            // Add other fields as needed
-        }
-
-        struct Fare: Codable {
-            let paymentFees: [PaymentFee]
-            let id: String
-            let price: Price
-            let tripId: String            // Add other fields as needed
-        }
-
-        struct Airline: Codable {
-            let name: String
-            let code: String
-            // Add other fields as needed
-        }
-
-        struct Airport: Codable {
-            let name: String
-            let code: String
-            let cityCode: String
-            // Add other fields as needed
-        }
-
-        struct City: Codable {
-            let code: String
-            let name: String
-            // Add other fields as needed
-        }
-
-        struct PaymentFee: Codable {
-            let paymentMethodId: Int
-            let currencyCode: String
-            let amount: Double
-            let amountUsd: Double
-            let totalAmount: Double
-            let totalAmountUsd: Double           
-            // Add other fields as needed
-        }
-
-        struct Price: Codable {
-            let totalAmount: Double
-            let totalAmountUsd: Double
-            // Add other fields as needed
-        }
-       /* struct DisplayFlightInfo {
-            let airlineName: String
-            let stopoversCount: Int
-            let totalDuration: String
-            let totalAmountUsd: Double
-        }*/
+        let legs: [Leg]
+        let trips: [Trip]
+        let fares: [Fare]
+        let airlines: [Airline]
+        let airports: [Airport]
+        // Add other fields as needed
+    }
+    
+    struct Leg: Codable {
+        let id: String
+        let departureTime: String
+        let arrivalTime: String
+        let duration: String
+        let departureAirportCode: String
+        let arrivalAirportCode: String
+        let airlineCodes: [String]
+        let stopoverAirportCodes: [String]
+        let allianceCodes: [String]
+        let stopoversCount: Int
+        let segments: [Segment]
+        // Add other fields as needed
+    }
+    
+    struct Segment: Codable {
+        let durationMinutes: Int
+        let stopoverDurationMinutes: Int
+        let departureAirportCode: String
+        let arrivalAirportCode: String
+        let airlineCode: String
+        let cabin: String
+        let designatorCode: String
+        // Add other fields as needed
+    }
+    
+    struct Trip: Codable {
+        let id: String
+        let code: String
+        let legIds: [String]
+        // Add other fields as needed
+    }
+    
+    struct Fare: Codable {
+        let paymentFees: [PaymentFee]
+        let id: String
+        let price: Price
+        let tripId: String            // Add other fields as needed
+    }
+    
+    struct Airline: Codable {
+        let name: String
+        let code: String
+        // Add other fields as needed
+    }
+    
+    struct Airport: Codable {
+        let name: String
+        let code: String
+        let cityCode: String
+        // Add other fields as needed
+    }
+    
+    struct City: Codable {
+        let code: String
+        let name: String
+        // Add other fields as needed
+    }
+    
+    struct PaymentFee: Codable {
+        let paymentMethodId: Int
+        let currencyCode: String
+        let amount: Double
+        let amountUsd: Double
+        let totalAmount: Double
+        let totalAmountUsd: Double
+        // Add other fields as needed
+    }
+    
+    struct Price: Codable {
+        let totalAmount: Double
+        let totalAmountUsd: Double
+        // Add other fields as needed
+    }
+    /* struct DisplayFlightInfo {
+     let airlineName: String
+     let stopoversCount: Int
+     let totalDuration: String
+     let totalAmountUsd: Double
+     }*/
     
     var cabinClass: String?
     var fromLocation: String?
@@ -141,86 +141,86 @@ class OneWayDisplayViewController: UIViewController,UITableViewDelegate, UITable
     }
     
     private func fetchDataFromAPI() {
-                guard let requestURL = createRequestURL() else {
-                    print("Failed to create API request URL.")
-                    return
+        guard let requestURL = createRequestURL() else {
+            print("Failed to create API request URL.")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: requestURL) { [weak self] data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    // Handle the error, e.g., show an alert to the user
+                    self?.handleError(error)
                 }
-
-                URLSession.shared.dataTask(with: requestURL) { [weak self] data, response, error in
-                    if let error = error {
-                        DispatchQueue.main.async {
-                            // Handle the error, e.g., show an alert to the user
-                            self?.handleError(error)
-                        }
-                        return
-                    }
-                    
-                    guard let data = data else {
-                        DispatchQueue.main.async {
-                            // Handle the no data case, e.g., show an error message to the user
-                            self?.handleNoDataError()
-                        }
-                        return
-                    }
-                    
-                    // Parse the JSON data
-                    guard let flightInfoArray = self?.parseFlightInfo(data: data) else { return }
-
-                                DispatchQueue.main.async {
-                                    self?.displayFlightInfoArray = flightInfoArray
-                                    self?.onewaydisplaytable.reloadData()
-                                }
-                            }.resume()
-           }
-
-            // Helper methods to handle different errors
-            private func handleError(_ error: Error) {
-                // Present an alert or update the UI to show the error
-                print("Error fetching data: \(error.localizedDescription)")
+                return
             }
-
-            private func handleNoDataError() {
-                // Present an alert or update the UI to indicate no data was received
-                print("No data received from the API.")
-            }
-
-            private func handleParsingError() {
-                // Present an alert or update the UI to show that there was a parsing error
-                print("Failed to parse flight information.")
-            }
-
-        private func createRequestURL() -> URL? {
-                guard let cabinClass = cabinClass,
-                      let fromLocation = fromLocation,
-                      let toLocation = toLocation,
-                      let departureDate = departureDate else {
-                    print("One or more required parameters are missing.")
-                    return nil
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    // Handle the no data case, e.g., show an error message to the user
+                    self?.handleNoDataError()
                 }
-                
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "yyyy-MM-dd"
-                let departureDateString = dateFormatter.string(from: departureDate)
-                
-                let urlString = "https://api.flightapi.io/onewaytrip/65395e4d01b26894ef8d6f94/\(fromLocation)/\(toLocation)/\(departureDateString)/1/0/0/\(cabinClass)/USD"
-                
-                return URL(string: urlString)
+                return
+            }
+            
+            // Parse the JSON data
+            guard let flightInfoArray = self?.parseFlightInfo(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self?.displayFlightInfoArray = flightInfoArray
+                self?.onewaydisplaytable.reloadData()
+            }
+        }.resume()
     }
     
- 
+    // Helper methods to handle different errors
+    private func handleError(_ error: Error) {
+        // Present an alert or update the UI to show the error
+        print("Error fetching data: \(error.localizedDescription)")
+    }
+    
+    private func handleNoDataError() {
+        // Present an alert or update the UI to indicate no data was received
+        print("No data received from the API.")
+    }
+    
+    private func handleParsingError() {
+        // Present an alert or update the UI to show that there was a parsing error
+        print("Failed to parse flight information.")
+    }
+    
+    private func createRequestURL() -> URL? {
+        guard let cabinClass = cabinClass,
+              let fromLocation = fromLocation,
+              let toLocation = toLocation,
+              let departureDate = departureDate else {
+            print("One or more required parameters are missing.")
+            return nil
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let departureDateString = dateFormatter.string(from: departureDate)
+        
+        let urlString = "https://api.flightapi.io/onewaytrip/65395e4d01b26894ef8d6f94/\(fromLocation)/\(toLocation)/\(departureDateString)/1/0/0/\(cabinClass)/USD"
+        
+        return URL(string: urlString)
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return displayFlightInfoArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "onewaycell", for: indexPath) as! OneWayDisplayTableViewCell
-
+        
         let displayFlightInfo = displayFlightInfoArray[indexPath.row]
         cell.currency.text = String(format: "$%.2f", displayFlightInfo.totalAmountUsd)
         cell.stops.text = "\(displayFlightInfo.stopoversCount) Stops"
         cell.totalDuration.text = "\(displayFlightInfo.totalDuration)"
         cell.airlinesName.text = displayFlightInfo.airlineName
-
+        
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -239,48 +239,48 @@ class OneWayDisplayViewController: UIViewController,UITableViewDelegate, UITable
             }
         }
     }
-
+    
     private func presentNoDetailsAlert() {
-           let alert = UIAlertController(title: "No Details Available", message: "There are no flight details available for the selected departure and destination.", preferredStyle: .alert)
-           alert.addAction(UIAlertAction(title: "OK", style: .default))
-           self.present(alert, animated: true)
-       }
-
-       // Present an alert when there's an error
+        let alert = UIAlertController(title: "No Details Available", message: "There are no flight details available for the selected departure and destination.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+    
+    // Present an alert when there's an error
     private func presentErrorAlert(message: String) {
-           let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-           alert.addAction(UIAlertAction(title: "OK", style: .default))
-           self.present(alert, animated: true)
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
     }
     private func parseFlightInfo(data: Data) -> [DisplayFlightInfo]? {
         do {
             let flightInfo = try JSONDecoder().decode(FlightInfo.self, from: data)
             
             guard !flightInfo.trips.isEmpty, !flightInfo.fares.isEmpty else {
-                           DispatchQueue.main.async {
-                               self.presentNoDetailsAlert()
-                           }
-                           return nil
+                DispatchQueue.main.async {
+                    self.presentNoDetailsAlert()
+                }
+                return nil
             }
             
             var displayFlightInfoArray: [DisplayFlightInfo] = []
             
             for trip in flightInfo.trips {
                 guard let firstLegId = trip.legIds.first,
-                let leg = flightInfo.legs.first(where: { $0.id == firstLegId }) else {
-                continue
-            }
-            
-               // let firstSegment = leg.segments.first
+                      let leg = flightInfo.legs.first(where: { $0.id == firstLegId }) else {
+                    continue
+                }
+                
+                // let firstSegment = leg.segments.first
                 let airlineCode = leg.segments.first?.airlineCode ?? "Unknown"
                 let airline = flightInfo.airlines.first(where: { $0.code == airlineCode }) ?? Airline(name: "Unknown", code: "Unknown")
-
+                
                 let totalDurationMinutes = leg.segments.reduce(0) { $0 + $1.durationMinutes }
                 let hours = totalDurationMinutes / 60
                 let minutes = totalDurationMinutes % 60
                 let totalDuration = "\(hours)h \(minutes)m"
                 let departureAirport = flightInfo.airports.first(where: { $0.code == leg.departureAirportCode }) ?? Airport(name: "Unknown", code: "Unknown", cityCode: "Unknown")
-
+                
                 // Find the fare that matches the trip ID.
                 if let fare = flightInfo.fares.first(where: { $0.tripId == trip.id }) {
                     let totalAmountUsd = fare.price.totalAmountUsd
@@ -289,7 +289,7 @@ class OneWayDisplayViewController: UIViewController,UITableViewDelegate, UITable
                         airlineName: airline.name,
                         stopoversCount: stopoversCount,
                         totalDuration: totalDuration,
-                        totalAmountUsd: totalAmountUsd, 
+                        totalAmountUsd: totalAmountUsd,
                         departureAirport: departureAirport.name
                     )
                     displayFlightInfoArray.append(displayFlightInfo)
@@ -297,7 +297,7 @@ class OneWayDisplayViewController: UIViewController,UITableViewDelegate, UITable
                     print("No fare found for trip ID: \(trip.id)")
                 }
             }
-
+            
             return displayFlightInfoArray
         } catch {
             print("Error parsing JSON: \(error)")
@@ -305,30 +305,38 @@ class OneWayDisplayViewController: UIViewController,UITableViewDelegate, UITable
         }
         
     }
-
+    
     @IBAction func filterByCheapest(_ sender: Any) {
         
         displayFlightInfoArray.sort { $0.totalAmountUsd < $1.totalAmountUsd }
-
-                    onewaydisplaytable.reloadData()
-
+        
+        onewaydisplaytable.reloadData()
+        
     }
     
     @IBAction func filterByLeastStops(_ sender: Any) {
         
         displayFlightInfoArray.sort { $0.stopoversCount < $1.stopoversCount }
-
-            onewaydisplaytable.reloadData()
+        
+        onewaydisplaytable.reloadData()
     }
     
     @IBAction func filterByAirlineName(_ sender: Any) {
-      
-            displayFlightInfoArray.sort { $0.airlineName < $1.airlineName }
-
-            onewaydisplaytable.reloadData()
-        }
-
+        
+        displayFlightInfoArray.sort { $0.airlineName < $1.airlineName }
+        
+        onewaydisplaytable.reloadData()
     }
+    
+    
+    
+    @IBAction func filterByShortestDuration(_ sender: Any) {
+        
+        
+    }
+    
+}
+
 
 
     
