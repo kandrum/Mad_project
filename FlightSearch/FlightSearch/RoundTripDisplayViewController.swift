dd048c1369e33ec7c62cc860bba9c6ab97f0e06d
Sri Nikesh Tamma <143757959+TSnr2000@users.noreply.github.com>
2023-12-06 21:24:40 -0600

    API changed

--- FlightSearch/FlightSearch/RoundTripDisplayViewController.swift
+++ FlightSearch/FlightSearch/RoundTripDisplayViewController.swift
@@ -185,7 +185,7 @@ class RoundTripDisplayViewController: UIViewController, UITableViewDelegate, UIT
         let departureDateString = dateFormatter.string(from: departureDateRound)
         let returnDateString = dateFormatter.string(from: returnDateRound)
         
-        let urlString = "https://api.flightapi.io/roundtrip/6564f5d42a16c63beb7af6b0/\(fromLocationRound)/\(toLocationRound)/\(departureDateString)/\(returnDateString)/1/0/1/\(cabinClassRound)/USD"
+        let urlString = "https://api.flightapi.io/roundtrip/6570facdc6eb315e7eecc451/\(fromLocationRound)/\(toLocationRound)/\(departureDateString)/\(returnDateString)/1/0/1/\(cabinClassRound)/USD"
         
         
         
