0ca7adc0fdc76009cfc1d8eba4cc24593676f843
SaiPoojithaV <143758110+SaiPoojithaV@users.noreply.github.com>
2023-12-07 08:23:37 -0600

    added Gradient layed for background and colour

--- FlightSearch/FlightSearch/RoundTripSelectedDetailViewController.swift
+++ FlightSearch/FlightSearch/RoundTripSelectedDetailViewController.swift
@@ -11,10 +11,23 @@ class RoundTripSelectedDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addGradientLayer()
 
         // Do any additional setup after loading the view.
     }
     
-
+    private func addGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        let skyBlueColor = UIColor(red: 135/255, green: 206/255, blue: 235/255, alpha: 1.0)
+        
+        gradientLayer.colors = [UIColor.white.cgColor, skyBlueColor.cgColor]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 
 }
