d1183628cce10938d003c02e7a8b2c75015f666e
yaswanthzz <143757849+yaswanthzz@users.noreply.github.com>
2023-12-07 00:10:00 -0600

    Revert "Duplicate API Code removed"
    This reverts commit 2113f977c2c5997988f51fac16d4c9902ff031a7.

--- FlightSearch/FlightSearch/OneWayDisplayViewController.swift
+++ FlightSearch/FlightSearch/OneWayDisplayViewController.swift
@@ -10,6 +10,97 @@ import UIKit
 class OneWayDisplayViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
     
     var selectedFlightInfo: DisplayFlightInfo?
+    
+//    struct FlightInfo: Codable {
+//        let legs: [Leg]
+//        let trips: [Trip]
+//        let fares: [Fare]
+//        let airlines: [Airline]
+//        let airports: [Airport]
+//        // Add other fields as needed
+//    }
+//    
+//    struct Leg: Codable {
+//        let id: String
+//        let departureTime: String
+//        let arrivalTime: String
+//        let duration: String
+//        let departureAirportCode: String
+//        let arrivalAirportCode: String
+//        let airlineCodes: [String]
+//        let stopoverAirportCodes: [String]
+//        let allianceCodes: [String]
+//        let stopoversCount: Int
+//        let segments: [Segment]
+//        // Add other fields as needed
+//    }
+//    
+//    struct Segment: Codable {
+//        let durationMinutes: Int
+//        let stopoverDurationMinutes: Int
+//        let departureAirportCode: String
+//        let arrivalAirportCode: String
+//        let airlineCode: String
+//        let cabin: String
+//        let designatorCode: String
+//        // Add other fields as needed
+//    }
+//    
+//    struct Trip: Codable {
+//        let id: String
+//        let code: String
+//        let legIds: [String]
+//        // Add other fields as needed
+//    }
+//    
+//    struct Fare: Codable {
+//        let paymentFees: [PaymentFee]
+//        let id: String
+//        let price: Price
+//        let tripId: String            // Add other fields as needed
+//    }
+//    
+//    struct Airline: Codable {
+//        let name: String
+//        let code: String
+//        // Add other fields as needed
+//    }
+//    
+//    struct Airport: Codable {
+//        let name: String
+//        let code: String
+//        let cityCode: String
+//        // Add other fields as needed
+//    }
+//    
+//    struct City: Codable {
+//        let code: String
+//        let name: String
+//        // Add other fields as needed
+//    }
+//    
+//    struct PaymentFee: Codable {
+//        let paymentMethodId: Int
+//        let currencyCode: String
+//        let amount: Double
+//        let amountUsd: Double
+//        let totalAmount: Double
+//        let totalAmountUsd: Double
+//        // Add other fields as needed
+//    }
+//    
+//    struct Price: Codable {
+//        let totalAmount: Double
+//        let totalAmountUsd: Double
+//        // Add other fields as needed
+//    }
+    /* struct DisplayFlightInfo {
+     let airlineName: String
+     let stopoversCount: Int
+     let totalDuration: String
+     let totalAmountUsd: Double
+     }*/
+    
     var cabinClass: String?
     var fromLocation: String?
     var toLocation: String?
